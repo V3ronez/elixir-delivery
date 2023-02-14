e253af99e7b1dbd38485a734942cf30d126c5640
Henrique Veronez <v3ronez.dev@gmail.com>
2023-02-14 09:46:27 -0300

    facade create_update order

--- lib/delivery.ex
+++ lib/delivery.ex
@@ -1,7 +1,14 @@
 defmodule Delivery do
-  alias Delivery.User.CreateOrUpdate
+  alias Delivery.Order
+  alias Delivery.Order.CreateOrUpdate, as: CreateOrUpdateOrder
+  alias Delivery.User.CreateOrUpdate, as: CreateOrUpdateUser
   alias Delivery.User.UserAgent
 
-  def start_links(), do: UserAgent.start_link(%{})
-  defdelegate create_or_update(params), to: CreateOrUpdate, as: :call
+  def start_agents() do
+    Order.OrderAgent.start_link(%{})
+    UserAgent.start_link(%{})
+  end
+
+  defdelegate create_or_update_user(params), to: CreateOrUpdateUser, as: :call
+  defdelegate create_or_update_order(params), to: CreateOrUpdateOrder, as: :call
 end
